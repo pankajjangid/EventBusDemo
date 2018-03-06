315a0291d9c643ab6880b065bc2719d885d77995
pankaj <pankaj0619@gmail.com>
2018-03-06 16:39:50 +0530

    First Change

--- app/src/main/java/com/pankaj/eventbusdemo/fragments/FragmentTwo.java
+++ app/src/main/java/com/pankaj/eventbusdemo/fragments/FragmentTwo.java
@@ -1,19 +1,30 @@
 package com.pankaj.eventbusdemo.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pankaj.eventbusdemo.R;
+import com.pankaj.eventbusdemo.SendEvent;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentTwo extends Fragment {
+public class FragmentTwo extends Fragment implements View.OnClickListener {
 
+    View mainView;
+    Context mContext;
+    Button btnSend;
+    EditText etVal;
 
     public FragmentTwo() {
         // Required empty public constructor
@@ -24,7 +35,40 @@ public class FragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        mainView = inflater.inflate(R.layout.fragment_two, container, false);
+        findControls();
+        return mainView;
+    }
+
+    private void findControls() {
+        mContext = getActivity();
+        EventBus.getDefault().register(this);
+        btnSend = mainView.findViewById(R.id.btnSend);
+        etVal = mainView.findViewById(R.id.etVal);
+
+        btnSend.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btnSend:
+                SendEvent sendEvent = new SendEvent(etVal.getText().toString());
+                // Post the event
+
+                EventBus.getDefault().post(sendEvent);
+
+                break;
+        }
+    }
+
+    @Subscribe
+    public void onFragmentOnClick(SendEvent sendEvent) {
+
+        etVal.setText(sendEvent.getMessage());
     }
 
 }
